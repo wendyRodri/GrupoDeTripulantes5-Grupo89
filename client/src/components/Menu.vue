a25de153712cc6afbb4b3a1aa5c6ca5dae4bd15e
faorozco <faorozco@utp.edu.co>
2021-10-10 00:03:06 -0500

    devolviendo categorías desde back

--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -1,84 +1,93 @@
 <template>
-  <div class= "ui secondary menu">
-      <div class="ui container">
-          <div class="left menu">
-            <router-link class="item" to="/">
-                <img class="ui small image" src="../assets/logo.png" alt="Ecommerce" />
-                <p>Categorías...</p>
-            </router-link>
-          </div>
-          <div class="right menu">
-             <router-link class="item" to="/login" v-if="!token">
-               Iniciar Sesión
-             </router-link>
-
-              <template v-if="token">
-                <router-link class="item" to="/orders">Pedidos</router-link>
-                <span class="ui item cart">
-                  <i class="shopping cart icon"></i>
-                </span>
-                <span class="ui item logout" @click="logout">
-                  <i class="sign-out icon"></i>
-                </span>
-              </template>
-          </div>
+  <div class="ui secondary menu">
+    <div class="ui container">
+      <div class="left menu">
+        <router-link class="item" to="/">
+          <img
+            class="ui small image"
+            src="../assets/logo.png"
+            alt="Ecommerce"
+          />
+          <p>Categorías...</p>
+        </router-link>
       </div>
+      <div class="right menu">
+        <router-link class="item" to="/login" v-if="!token">
+          Iniciar Sesión
+        </router-link>
+
+        <template v-if="token">
+          <router-link class="item" to="/orders">Pedidos</router-link>
+          <span class="ui item cart">
+            <i class="shopping cart icon"></i>
+          </span>
+          <span class="ui item logout" @click="logout">
+            <i class="sign-out icon"></i>
+          </span>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {getTokenApi} from '../api/token';
+import { ref, onMounted } from 'vue';
+import { getTokenApi, deleteTokenApi } from "../api/token";
+import { getCategoriesApi } from '../api/category';
 
 export default {
-    name: "Menu",
+  name: "Menu",
 
+  setup() {
+    let categories = ref(null);
 
+    onMounted(async () => {
+      const response = await getCategoriesApi();
+      console.log(response);
+    });
 
-setup() {
-  const token = getTokenApi();
+    const token = getTokenApi();
 
-  const logout = () => {
-    console.log('Cerar sesión');
-  };
+    const logout = () => {
+      deleteTokenApi();
+      location.replace('/');
+      console.log("Cerrar sesión");
+    };
 
-  return {
-    token,
-    logout
-  };
-},
+    return {
+      token,
+      logout,
+      categories,
+    };
+  },
 };
-
-
-
 </script>
 
 <style lang="scss" scoped>
-.ui.menu.secondary{
-    background-color:#16202b;
-    .item{
-        color:#ffffff;
-        &:hover{
-            color:#1fa1f1;
-        }
+.ui.menu.secondary {
+  background-color: #16202b;
+  .item {
+    color: #ffffff;
+    &:hover {
+      color: #1fa1f1;
     }
-    .menu.left{
-        width: 50%;
-        .ui.image{
-            width: 70px;
-        }
+  }
+  .menu.left {
+    width: 50%;
+    .ui.image {
+      width: 70px;
     }
-    .menu.right{
-        width:50% ;
-        justify-content: flex-end;
-        
-        .logout,
-        .cart{
-            
-            &:hover{
-                cursor: pointer;
-            }
-        }
-    }
-}
+  }
+  .menu.right {
+    width: 50%;
+    justify-content: flex-end;
 
+    .logout,
+    .cart {
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+}
 </style>
