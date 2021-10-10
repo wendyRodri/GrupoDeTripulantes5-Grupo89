b533d0d2187fe68f996a374d65548c8c6bc07162
faorozco <faorozco@utp.edu.co>
2021-10-10 16:20:34 -0500

    estilo al carrito

--- client/src/components/Cart/Cart.vue
+++ client/src/components/Cart/Cart.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="cart-dimmer" :class="{ open: showCart }" @click="closeCart" />
   <div class="cart" :class="{ open: showCart }">
     <h2>Esto es el carrito</h2>
     <button @click="closeCart">Cerrar</button>
@@ -7,43 +8,60 @@
 
 
 <script>
-import { computed } from 'vue';
-import { useStore } from 'vuex';
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
-  name: 'Cart',
+  name: "Cart",
 
   setup() {
-      const store = useStore();
-      const showCart = computed(() => store.state.showCart);
+    const store = useStore();
+    const showCart = computed(() => store.state.showCart);
 
-      const closeCart =()=>{
-        store.commit('setShowCart', false);
-      };
+    const closeCart = () => {
+      store.commit("setShowCart", false);
+    };
 
-      return {
-        showCart,
-        closeCart,
-      };
+    return {
+      showCart,
+      closeCart,
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.cart {
+.cart-dimmer {
+  opacity: 0;
+  transition: opacity 0.5 ease;
+  &.open {
     position: fixed;
-    right: 0;
     top: 0;
-    width: 400px;
-    height: 100vh;
-    background-color: #fff;
-    box-shadow: 0px 0px 26px 5px rgba(0, 0, 0, 0.75);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    transition: transform 0.9s ease;
-    transform: translateX(150%);
-    &.open {
-        transform: translateX(0);
-    }
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000;
+    opacity: 0.7;
+  }
+}
+
+
+
+
+.cart {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 400px;
+  height: 100vh;
+  background-color: #fff;
+  box-shadow: 0px 0px 26px 5px rgba(0, 0, 0, 0.75);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: transform 0.9s ease;
+  transform: translateX(150%);
+  &.open {
+    transform: translateX(0);
+  }
 }
 </style>
