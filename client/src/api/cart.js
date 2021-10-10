669892f0cf91aea968a43ca37fea48f6af571e8e
faorozco <faorozco@utp.edu.co>
2021-10-10 18:23:47 -0500

    obtenidos productos del carrito

--- client/src/api/cart.js
+++ client/src/api/cart.js
@@ -1,3 +1,4 @@
+import { uniqBy, countBy } from "lodash";
 import  { API_URL, PRODUCTS } from "../utils/constants";
 
 export function addProductCartApi(idProduct){
@@ -13,4 +14,35 @@ export function getCartApi() {
     if (!products) return [];
 
     return JSON.parse(products);
+}
+
+export async function getProductsCartApi() {
+    const idProducts = getCartApi();
+
+    if (idProducts.lenght === 0) return null;
+
+    try {
+        const products = [];
+
+        for await (const idProduct of idProducts) {
+            const response = await fetch(`${API_URL}/products/${idProduct}`);
+            const result = await response.json();
+            products.push(result);
+    }
+
+    const productsCount = countBy(products, (product) => {
+        return product.name;
+    });
+
+    const combined = uniqBy(products, (product) => {
+        const productTemp = product;
+        productTemp.quantity = productsCount[product.name];
+        return productTemp.name;
+    });
+
+    return combined;
+    }   catch (error) {
+        console.log(error);
+        return null;
+    }
 }
--- client/src/components/Cart/Cart.vue
+++ client/src/components/Cart/Cart.vue
@@ -9,9 +9,10 @@
 
 
 <script>
-import { computed } from "vue";
+import { ref, computed, watchEffect } from "vue";
 import { useStore } from "vuex";
 import CartHeader from './CartHeader.vue';
+import { getProductsCartApi } from '../../api/cart';
 
 export default {
   name: "Cart",
@@ -22,6 +23,18 @@ export default {
   setup() {
     const store = useStore();
     const showCart = computed(() => store.state.showCart);
+    let products = ref(null);
+
+    const getProductsCart = async () => {
+      const response = await getProductsCartApi();
+      products.value = response;
+    };
+
+    watchEffect(() => {
+      showCart.value;
+      getProductsCart();
+    });
+
 
     const closeCart = () => {
       store.commit("setShowCart", false);
